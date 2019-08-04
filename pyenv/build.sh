a678c70456613f56cd6e6cf56b4409fdd500eeb8
Hodaka Suzuki <altitude3190@gmail.com>
2019-08-04 13:20:41 +0900

    Update build.sh

--- pyenv/build.sh
+++ pyenv/build.sh
@@ -10,4 +10,5 @@ pyenv global $version
 # https://qiita.com/Neetless/items/160fcc126c17590b664a#shim%E3%81%A8%E3%83%90%E3%83%BC%E3%82%B8%E3%83%A7%E3%83%B3%E5%88%87%E3%82%8A%E6%9B%BF%E3%81%88%E3%81%AE%E4%BB%95%E7%B5%84%E3%81%BF
 pyenv rehash
 
+pip install --upgrade pip
 pip3 install pipenv # pipfileに記載されたmoduleのインストール
