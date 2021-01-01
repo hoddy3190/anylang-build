ef3c6a59a0fed7c4814542dbc1b10ff331e445d9
Hodaka Suzuki <altitude3190@gmail.com>
2021-01-02 00:38:47 +0900

    rehashに関するコメント追記

--- nodenv/build.sh
+++ nodenv/build.sh
@@ -34,4 +34,12 @@ nodenv global $version
 
 # $NODENV_ROOT/shimsに配置されるコマンドを現在のversionにあわせて再編成するコマンド
 # https://qiita.com/Neetless/items/160fcc126c17590b664a#shim%E3%81%A8%E3%83%90%E3%83%BC%E3%82%B8%E3%83%A7%E3%83%B3%E5%88%87%E3%82%8A%E6%9B%BF%E3%81%88%E3%81%AE%E4%BB%95%E7%B5%84%E3%81%BF
+# ls $HOME/.anyenv/envs/nodenv/shims
+# browserify  <-- 12.18.1
+# node        <-- 12.18.1
+# npm         <-- 12.18.1
+# npx         <-- 12.18.1
+# textlint    <-- 12.14.1
+# 12.18.1でrehashしても12.14.1でインストールしたtextlintは残る
+# nodeやnpmは12.14.1でもあるがrehashすることにより12.18.1のものに差し替わる
 nodenv rehash
