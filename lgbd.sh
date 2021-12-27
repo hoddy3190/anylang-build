bd907eac97f7be76412df4380b12d45aeb7c085c
Hodaka Suzuki <altitude3190@gmail.com>
2021-12-28 00:05:24 +0900

    langbd.sh -> lgbd.sh

---
+++ lgbd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -euo pipefail
+
+lang="$1"
+ver=${2:-''}
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
+
+case "$lang" in
+    "go"   ) "$SCRIPT_DIR"/envs/"$lang"env/build.sh "$ver" ;;
+    "j"    ) "$SCRIPT_DIR"/envs/"$lang"env/build.sh "$ver" ;;
+    "node" ) "$SCRIPT_DIR"/envs/"$lang"nv/build.sh "$ver" ;;
+    "py"   ) "$SCRIPT_DIR"/envs/"$lang"env/build.sh "$ver" ;;
+    "rb"   ) "$SCRIPT_DIR"/envs/"$lang"env/build.sh "$ver" ;;
+    *      ) echo "$lang は対応していない言語です"; exit 1 ;;
+esac
