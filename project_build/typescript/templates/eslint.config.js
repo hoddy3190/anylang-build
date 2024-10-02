069a599f9435efce783a68898df5a71fb4c7c13e
Hodaka Suzuki <altitude3190@gmail.com>
2024-10-02 21:59:39 +0900

    feat: eslint.config.js boilerplate

---
+++ project_build/typescript/templates/eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  {
+    ignores: ["dist/**/*"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier
+);
