49aa8608051943b32bb53f820574c01c50a52cb9
Hodaka Suzuki <altitude3190@gmail.com>
2024-10-31 09:31:11 +0900

    fix: forward read commands

--- project_build/python/build.sh
+++ project_build/python/build.sh
@@ -4,21 +4,22 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
-if ! which pyenv > /dev/null 2>&1; then
-  ${SCRIPT_DIR}/pyenv.sh
-  exit 1
-fi
-
 read -p "Do you expand the boilerplates at \"$(pwd)\"? [y/n]: "
 if [[ "${REPLY}" != "y" ]]; then
   exit 0
 fi
 
-curl -L https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -o .gitignore
+read -p "What version do you use in this project(default: latest released version)? [x.y.z]: " input_version
+
+if ! which pyenv > /dev/null 2>&1; then
+  echo "install pyenv and pipenv"
+  ${SCRIPT_DIR}/pyenv.sh
+fi
 
 cur_version=$(pyenv version-name)
-read -p "What version do you use in this project (default: ${cur_version})? [x.y.z]: " input_version
 version=${input_version:-"${cur_version}"}
+
+curl -L https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -o .gitignore
 
 set +x
 pipenv --python "${version}"
