53d459a828a82cc6327ee143ff2baaa0ce8419a8
Hodaka Suzuki <altitude3190@gmail.com>
2024-11-01 16:14:15 +0900

    feat: install pylance, debugpy

--- project_build/python/vscode.sh
+++ project_build/python/vscode.sh
@@ -1,2 +1,6 @@
 # https://marketplace.visualstudio.com/items?itemName=ms-python.python
 code --install-extension ms-python.python --force
+# https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance
+code --install-extension ms-python.vscode-pylance --force
+# https://marketplace.visualstudio.com/items?itemName=ms-python.debugpy
+code --install-extension ms-python.debugpy --force
