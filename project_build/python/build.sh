20c0180bccdc2525cb82c655abdc480ced295574
Hodaka Suzuki <altitude3190@gmail.com>
2024-11-11 10:14:00 +0900

    feat: remove pipenv from build.sh

--- project_build/python/build.sh
+++ project_build/python/build.sh
@@ -11,16 +11,12 @@ fi
 
 read -p "What version do you use in this project(default: latest released version)? [x.y.z]: " input_version
 
-if ! which pyenv > /dev/null 2>&1; then
-  echo "install pyenv and pipenv"
-  ${SCRIPT_DIR}/pyenv.sh "$input_version"
-fi
-
-cur_version=$(pyenv version-name)
-version=${input_version:-"${cur_version}"}
-
 curl -L https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -o .gitignore
+curl -L https://raw.githubusercontent.com/hoddy3190/anylang-build/main/project_build/python/templates/Makefile -o Makefile
 
-set +x
-pipenv --python "${version}"
-set -x
+${SCRIPT_DIR}/vscode.sh
+
+${SCRIPT_DIR}/pyenv.sh "$input_version"
+# python -m venv .venv は、現在の python のバージョンで作られるため、
+# pyenv で使いたいバージョンを先にインストールし、global に設定する必要がある
+make venv
