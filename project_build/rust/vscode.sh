1377edcf8adda48df4a19c381524a79197b7f777
Hodaka Suzuki <altitude3190@gmail.com>
2025-02-16 11:57:01 +0900

    feat: rust settings.json

--- project_build/rust/vscode.sh
+++ project_build/rust/vscode.sh
@@ -1,2 +1,5 @@
 # https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer
 code --install-extension rust-lang.rust-analyzer
+
+mkdir -p .vscode
+curl -L https://raw.githubusercontent.com/hoddy3190/anylang-build/main/project_build/rust/templates/settings.json -o ./.vscode/settings.json
