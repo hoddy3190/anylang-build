76e1a4d27af2b88852e6ae5f6ac590b2b1cd44be
Hodaka Suzuki <altitude3190@gmail.com>
2025-03-16 09:54:12 +0900

    feat: Add source URL to the .gitignore file

--- project_build/python/build.sh
+++ project_build/python/build.sh
@@ -14,8 +14,15 @@ fi
 
 read -rp "What version do you use in this project(default: latest released version)? [x.y.z]: " input_version
 
-curl -L https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -o .gitignore
-curl -L https://raw.githubusercontent.com/hoddy3190/anylang-build/main/project_build/python/templates/Makefile -o Makefile
+latest_commit_hash=$(curl -sL "https://api.github.com/repos/github/gitignore/commits/main" | jq -r '.sha')
+cat <<EOF > .gitignore
+### Python ###
+# https://raw.githubusercontent.com/github/gitignore/${latest_commit_hash}/Python.gitignore
+
+EOF
+curl -sL https://raw.githubusercontent.com/github/gitignore/${latest_commit_hash}/Python.gitignore >> .gitignore
+
+curl -sL https://raw.githubusercontent.com/hoddy3190/anylang-build/main/project_build/python/templates/Makefile -o Makefile
 
 "${SCRIPT_DIR}"/vscode.sh
 
